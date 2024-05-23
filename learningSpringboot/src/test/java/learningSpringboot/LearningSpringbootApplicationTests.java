3833d5117fec7f147eb1f15024132ba0dde493c7
ysdhimself <150086936+ysdhimself@users.noreply.github.com>
2024-05-23 11:54:07 -0600

    updates

--- learningSpringboot/src/test/java/learningSpringboot/LearningSpringbootApplicationTests.java
+++ learningSpringboot/src/test/java/learningSpringboot/LearningSpringbootApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package learningSpringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class LearningSpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
